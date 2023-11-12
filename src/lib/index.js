4ec77c7f91cc7feebab0d259b7eba577a0ee6b56
michellejanay <mindyourphotosynthesis@gmail.com>
2023-11-12 22:58:32 +0000

    test: error fix?

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-
-import fs from "fs/promises";
-import path from "path";
-import process from "process";
-import { authenticate } from "@google-cloud/local-auth";
-import { google } from "googleapis";

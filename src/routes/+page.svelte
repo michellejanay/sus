c3b6bd8129b0031b73b958da41cc29474827614b
michellejanay <mindyourphotosynthesis@gmail.com>
2023-11-12 23:04:37 +0000

    feat: landing page start button style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
 
-<div class="flex justify-center">
-  <a href="/survey">Start</a>
+<div class="flex justify-center my-40">
+  <a href="/survey" class="px-16 text-2xl">Start</a>
 </div>
